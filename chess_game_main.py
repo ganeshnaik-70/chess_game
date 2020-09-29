c4e6517ed088eb8d34d19b68d509283377c6c383
ganeshnaik-70 <ganeshravinaik2001@gmail.com>
2020-09-29 19:55:58 +0530

    completed Rook class and its movement and collision case

--- chess_game_main.py
+++ chess_game_main.py
@@ -211,10 +211,20 @@ class Rook:
     def check_move(self, row, col):
         self.front_move(row, col)
         self.back_move(row, col)
+        self.left_move(row, col)
+        self.right_move(row, col)
         for obj in self.front_list:
             obj.clr = YELLOW
         for obj in self.back_list:
             obj.clr = YELLOW
+        for obj in self.left_list:
+            obj.clr = YELLOW
+        for obj in self.right_list:
+            obj.clr = YELLOW
+        self.front_list.clear()
+        self.back_list.clear()
+        self.left_list.clear()
+        self.right_list.clear()
 
     def front_move(self, row, col):
         if row > 6:
@@ -223,7 +233,7 @@ class Rook:
             if grid[row + 1][col].piece is None:
                 self.front_list.append(grid[row + 1][col])
                 self.front_move(row + 1, col)
-            elif grid[row + 1][col].piece is not None and grid[row + 1][col].piece.clor == WHITE:
+            elif grid[row + 1][col].piece is not None and self.check_opponent(grid[row + 1][col].piece.clor):
                 grid[row + 1][col].clr = RED
                 return
             else:
@@ -236,11 +246,58 @@ class Rook:
             if grid[row - 1][col].piece is None:
                 self.back_list.append(grid[row - 1][col])
                 self.back_move(row - 1, col)
-            elif grid[row - 1][col].piece is not None and grid[row - 1][col].piece.clor == WHITE:
+            elif grid[row - 1][col].piece is not None and self.check_opponent(grid[row - 1][col].piece.clor):
                 grid[row - 1][col].clr = RED
                 return
             else:
                 return
+
+    def left_move(self, row, col):
+        if col < 1:
+            return
+        else:
+            if grid[row][col - 1].piece is None:
+                self.left_list.append(grid[row][col - 1])
+                self.left_move(row, col - 1)
+            elif grid[row][col - 1].piece is not None and self.check_opponent(grid[row][col-1].piece.clor):
+                grid[row][col - 1].clr = RED
+                return
+            else:
+                return
+
+    def right_move(self, row, col):
+        if col > 6:
+            return
+        else:
+            if grid[row][col + 1].piece is None:
+                self.right_list.append(grid[row][col + 1])
+                self.right_move(row, col + 1)
+            elif grid[row][col + 1].piece is not None and self.check_opponent(grid[row][col+1].piece.clor):
+                grid[row][col + 1].clr = RED
+                return
+            else:
+                return
+
+    def move(self, row, col, collision):
+        self.px = col * gap + 10
+        self.py = row * gap + 10
+        if collision:
+            eliminate(row, col)
+        grid[row][col].piece = self
+        grid[self.row][self.col].piece = None
+        self.row = self.py // gap
+        self.col = self.px // gap
+        make_box(grid)
+
+    def check_opponent(self, op_clr):
+        if (self.clor == BLACK and op_clr == WHITE) or (self.clor == WHITE and op_clr == BLACK):
+            return True
+        else:
+            return False
+
+
+class White_rook(Rook):
+    pass
 
 
 def check_piece_move(grid, row, col):
@@ -272,8 +329,10 @@ def check_piece_move(grid, row, col):
 #    wp = White_pawn(10 + i * gap, 460, WHITE, white_pawn)
 #    black_pawn_list.append(bp)
 #    white_pawn_list.append(wp)
-rp = Rook(10, 10+75*7, BLACK, black_rook)
-wp = White_pawn(10, 85, WHITE, black_pawn)
+rp = Rook(10 + 75 * 5, 10 + 75 * 2, BLACK, black_rook)
+wp = White_rook(10, 85 + 75, WHITE, white_rook)
+wp2 = White_rook(10+75*7, 85 + 75, WHITE, white_rook)
+
 
 # main game loop
 running = True
@@ -304,7 +363,8 @@ while running:
 
     create_board()
     rp.show_rook()
-    wp.show_pawn()
+    wp.show_rook()
+    wp2.show_rook()
 
     # update the display screen
     pygame.display.update()
