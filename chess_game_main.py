253601c8c532006fece7cef251c9df492e79f798
ganeshnaik-70 <ganeshravinaik2001@gmail.com>
2020-09-30 13:54:56 +0530

    sapareted Rook class from main file and fixed some bugs in main file

--- chess_game_main.py
+++ chess_game_main.py
@@ -1,5 +1,6 @@
 import pygame
 import os
+from chess_game_project import Pawn_piece, Rook_piece
 
 os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (700, 70)
 # initialize pygame module
@@ -94,180 +95,44 @@ grid = create_grid()
 make_box(grid)
 
 
-
-
-
-
-
-
-
-
-
-class White_pawn(Pawn):
-    def check_move(self, row, col):
-        if row > 0:
-            self.check_diagonal(row, col)
-            if self.first_move:
-                if check_pawn_first_move(grid[row - 1][col]):
-                    if check_place(grid[row - 2][col]):
-                        grid[row - 2][col].clr = YELLOW
-            else:
-                check_pawn_first_move(grid[row - 1][col])
-
-    def check_diagonal(self, row, col):
-        if col > 0 and grid[row - 1][col - 1].piece is not None and grid[row - 1][col - 1].piece.clor == BLACK:  # left
-            grid[row - 1][col - 1].clr = RED
-            self.diagonal_move.append(grid[row - 1][col - 1])
-        if col < 7 and grid[row - 1][col + 1].piece is not None and grid[row - 1][col + 1].piece.clor == BLACK:  # right
-            grid[row - 1][col + 1].clr = RED
-            self.diagonal_move.append(grid[row - 1][col + 1])
-
-
-class Rook:
-    def __init__(self, p_x, p_y, clr, img):
-        self.px = p_x
-        self.py = p_y
-        self.row = p_y // gap
-        self.col = p_x // gap
-        self.front_list = []
-        self.back_list = []
-        self.left_list = []
-        self.right_list = []
-        grid[self.row][self.col].piece = self
-        self.eliminated = False
-        self.clor = clr
-        self.rook_img = img
-
-    # To show a rook on board
-    def show_rook(self):
-        if not self.eliminated:
-            screen.blit(self.rook_img, (self.px, self.py))
-
-    def check_move(self, row, col):
-        self.front_move(row, col)
-        self.back_move(row, col)
-        self.left_move(row, col)
-        self.right_move(row, col)
-        for obj in self.front_list:
-            obj.clr = YELLOW
-        for obj in self.back_list:
-            obj.clr = YELLOW
-        for obj in self.left_list:
-            obj.clr = YELLOW
-        for obj in self.right_list:
-            obj.clr = YELLOW
-        self.front_list.clear()
-        self.back_list.clear()
-        self.left_list.clear()
-        self.right_list.clear()
-
-    def front_move(self, row, col):
-        if row > 6:
-            return
-        else:
-            if grid[row + 1][col].piece is None:
-                self.front_list.append(grid[row + 1][col])
-                self.front_move(row + 1, col)
-            elif grid[row + 1][col].piece is not None and self.check_opponent(grid[row + 1][col].piece.clor):
-                grid[row + 1][col].clr = RED
-                return
-            else:
-                return
-
-    def back_move(self, row, col):
-        if row < 1:
-            return
-        else:
-            if grid[row - 1][col].piece is None:
-                self.back_list.append(grid[row - 1][col])
-                self.back_move(row - 1, col)
-            elif grid[row - 1][col].piece is not None and self.check_opponent(grid[row - 1][col].piece.clor):
-                grid[row - 1][col].clr = RED
-                return
-            else:
-                return
-
-    def left_move(self, row, col):
-        if col < 1:
-            return
-        else:
-            if grid[row][col - 1].piece is None:
-                self.left_list.append(grid[row][col - 1])
-                self.left_move(row, col - 1)
-            elif grid[row][col - 1].piece is not None and self.check_opponent(grid[row][col-1].piece.clor):
-                grid[row][col - 1].clr = RED
-                return
-            else:
-                return
-
-    def right_move(self, row, col):
-        if col > 6:
-            return
-        else:
-            if grid[row][col + 1].piece is None:
-                self.right_list.append(grid[row][col + 1])
-                self.right_move(row, col + 1)
-            elif grid[row][col + 1].piece is not None and self.check_opponent(grid[row][col+1].piece.clor):
-                grid[row][col + 1].clr = RED
-                return
-            else:
-                return
-
-    def move(self, row, col, collision):
-        self.px = col * gap + 10
-        self.py = row * gap + 10
-        if collision:
-            eliminate(row, col)
-        grid[row][col].piece = self
-        grid[self.row][self.col].piece = None
-        self.row = self.py // gap
-        self.col = self.px // gap
-        make_box(grid)
-
-    def check_opponent(self, op_clr):
-        if (self.clor == BLACK and op_clr == WHITE) or (self.clor == WHITE and op_clr == BLACK):
-            return True
-        else:
-            return False
-
-
-class White_rook(Rook):
-    pass
+# To eliminate emeny piece
+def eliminate(row, col, grid):
+    grid[row][col].piece.eliminated = True
+    grid[row][col].piece = None
 
 
 def check_piece_move(grid, row, col):
     if grid[row][col].piece is not None and (grid[row][col].clr == BLACK or grid[row][col].clr == WHITE):
         if len(piece_list) == 0:
-            grid[row][col].piece.check_move(row, col)
+            grid[row][col].piece.check_move(row, col, grid)
             piece_list.append(grid[row][col].piece)
         else:
             piece_list.pop()
             make_box(grid)
-            grid[row][col].piece.check_move(row, col)
+            grid[row][col].piece.check_move(row, col, grid)
             piece_list.append(grid[row][col].piece)
 
     elif grid[row][col].piece is None and grid[row][col].clr == YELLOW:
         # function to move the piece
-        piece_list[0].move(row, col, collision=False)
+        piece_list[0].move(row, col, grid, collision=False)
 
     elif grid[row][col].piece is not None and grid[row][col].clr == RED:
         # function to move the piece and eliminate enemy piece
-        piece_list[0].move(row, col, collision=True)
+        piece_list[0].move(row, col, grid, collision=True)
 
     elif grid[row][col].piece is None and (grid[row][col].clr == BLACK or grid[row][col].clr == WHITE):
         make_box(grid)
         piece_list.clear()
 
 
-# for i in range(8):
-#    bp = Pawn(10 + i * gap, 85, BLACK, black_pawn)
-wp = White_pawn(10 +  gap, 460, WHITE, white_pawn, grid)
-#    black_pawn_list.append(bp)
-#    white_pawn_list.append(wp)
-rp = Rook(10 + 75 * 5, 10 + 75 * 2, BLACK, black_rook)
-wp = White_rook(10, 85 + 75, WHITE, white_rook)
-wp2 = White_rook(10+75*7, 85 + 75, WHITE, white_rook)
-
+for i in range(8):
+    bp = Pawn_piece.Pawn(10 + i * gap, 85, BLACK, black_pawn, grid, screen)
+    wp = Pawn_piece.White_pawn(10 + i * gap, 460, WHITE, white_pawn, grid, screen)
+    black_pawn_list.append(bp)
+    white_pawn_list.append(wp)
+rp = Rook_piece.Rook(10 + 75 * 5, 10 + 75 * 2, BLACK, black_rook, grid, screen)
+wr = Rook_piece.Rook(10, 85 + 75, WHITE, white_rook, grid, screen)
+# wp2 = White_rook(10 + 75 * 7, 85 + 75, WHITE, white_rook)
 
 # main game loop
 running = True
@@ -292,14 +157,13 @@ while running:
         for j in range(8):
             grid[i][j].draw_box()
 
-    #    for i in range(len(black_pawn_list)):
-    #        black_pawn_list[i].show_pawn()
-    #        white_pawn_list[i].show_pawn()
+    for i in range(len(black_pawn_list)):
+        black_pawn_list[i].show_pawn()
+        white_pawn_list[i].show_pawn()
 
     create_board()
     rp.show_rook()
-    wp.show_rook()
-    wp2.show_rook()
+    wr.show_rook()
 
     # update the display screen
     pygame.display.update()
