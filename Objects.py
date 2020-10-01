5d523c82aab0e52da196504f7c144b5292594ab6
ganeshnaik-70 <ganeshravinaik2001@gmail.com>
2020-10-01 16:58:40 +0530

    initialized objects of piece class in saparate py file and imported it in main game file

---
+++ Objects.py
@@ -0,0 +1,73 @@
+import pygame
+from chess_game_project import Pawn_piece, Rook_piece, Knight_piece, King_piece, Bishop_piece, Queen_piece
+
+# constant values
+gap = 75
+BLACK = (139, 69, 45)
+WHITE = (250, 235, 215)
+
+pawn_list = []
+bishop_list = []
+rook_list = []
+knight_list = []
+king_list = []
+queen_list = []
+
+# load all piece images
+black_pawn = pygame.image.load("images/black_pawn.png")
+black_rook = pygame.image.load("images/black_rook.png")
+black_bishop = pygame.image.load("images/black_bishop.png")
+black_king = pygame.image.load("images/black_king.png")
+black_queen = pygame.image.load("images/black_queen.png")
+black_horse = pygame.image.load("images/black_horse.png")
+white_pawn = pygame.image.load("images/white_pawn.png")
+white_rook = pygame.image.load("images/white_rook.png")
+white_bishop = pygame.image.load("images/white_bishop.png")
+white_king = pygame.image.load("images/white_king.png")
+white_queen = pygame.image.load("images/white_queen.png")
+white_horse = pygame.image.load("images/white_horse.png")
+
+
+def obj_init(grid, screen):
+    for i in range(8):
+        bpp = Pawn_piece.Pawn(10 + i * gap, 85, BLACK, black_pawn, grid, screen)
+        wpp = Pawn_piece.White_pawn(10 + i * gap, 460, WHITE, white_pawn, grid, screen)
+        pawn_list.append(bpp)
+        pawn_list.append(wpp)
+
+    brp1 = Rook_piece.Rook(10, 10, BLACK, black_rook, grid, screen)
+    brp2 = Rook_piece.Rook(10+75*7, 10, BLACK, black_rook, grid, screen)
+    wrp1 = Rook_piece.Rook(10, 10+75*7, WHITE, white_rook, grid, screen)
+    wrp2 = Rook_piece.Rook(10+75*7, 10+75*7, WHITE, white_rook, grid, screen)
+    rook_list.append(brp1)
+    rook_list.append(brp2)
+    rook_list.append(wrp1)
+    rook_list.append(wrp2)
+
+    bkp1 = Knight_piece.Knight(10 + 75, 10, BLACK, black_horse, grid, screen)
+    bkp2 = Knight_piece.Knight(10 + 75 * 6, 10, BLACK, black_horse, grid, screen)
+    wkp1 = Knight_piece.Knight(10 + 75, 10+75*7, WHITE, white_horse, grid, screen)
+    wkp2 = Knight_piece.Knight(10 + 75*6, 10+75*7, WHITE, white_horse, grid, screen)
+    knight_list.append(bkp1)
+    knight_list.append(bkp2)
+    knight_list.append(wkp1)
+    knight_list.append(wkp2)
+
+    bbp1 = Bishop_piece.Bishop(10+75*2, 10, BLACK, black_bishop, grid, screen)
+    bbp2 = Bishop_piece.Bishop(10+75*5, 10, BLACK, black_bishop, grid, screen)
+    wbp1 = Bishop_piece.Bishop(10+75*2, 10+75*7, WHITE, white_bishop, grid, screen)
+    wbp2 = Bishop_piece.Bishop(10+75*5, 10+75*7, WHITE, white_bishop, grid, screen)
+    bishop_list.append(bbp1)
+    bishop_list.append(bbp2)
+    bishop_list.append(wbp1)
+    bishop_list.append(wbp2)
+
+    bqp = Queen_piece.Queen(10 + 75 * 3, 10, BLACK, black_queen, grid, screen)
+    wqp = Queen_piece.Queen(10 + 75 * 3, 10 + 75 * 7, WHITE, white_queen, grid, screen)
+    queen_list.append(bqp)
+    queen_list.append(wqp)
+
+    bkp = King_piece.King(10+75*4, 10, BLACK, black_king, grid, screen)
+    wkp = King_piece.King(10+75*4, 10+75*7, WHITE, white_king, grid, screen)
+    king_list.append(bkp)
+    king_list.append(wkp)
